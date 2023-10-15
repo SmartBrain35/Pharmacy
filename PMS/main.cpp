a2ff7a51073f6bd107d98e85e70ae91bb99d7f26
SmartBrain35 <ramsonrutherford@gmail.com>
2023-10-15 14:09:36 +0000

    program starting point file

---
+++ PMS/main.cpp
@@ -0,0 +1,18 @@
+#include "home.h"
+#include "part_1.h"
+#include "stock.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+
+    Part_1 app;
+
+
+
+    app.show();
+
+    return a.exec();
+}
